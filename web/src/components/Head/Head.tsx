19cd45fb9e078dba167306f00b536a8c8e74d0f5
AlvSovereign <web@alvinsovereign.com>
2021-03-01 18:17:46 +0000

    OpenGraph meta tags

--- web/src/components/Head/Head.tsx
+++ web/src/components/Head/Head.tsx
@@ -12,6 +12,31 @@ const Head: FC = () => {
         name='description'
         content='Alvin Sovereign is a London-Based Web Developer specialising in the front end.'
       />
+
+      <meta property='og:url' content='https://alvinsovereign.com' />
+      <meta property='og:type' content='website' />
+      <meta
+        property='og:title'
+        content='Alvin Sovereign | Maker, Breaker and Tester of all things UI'
+      />
+      <meta
+        property='og:description'
+        content='Alvin Sovereign is a London-Based Web Developer specialising in the front end.'
+      />
+      <meta property='og:image' content='/images/open_graph_headshot.jpg' />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='twitter:domain' content='alvinsovereign.com' />
+      <meta property='twitter:url' content='https://alvinsovereign.com' />
+      <meta
+        name='twitter:title'
+        content='Alvin Sovereign | Maker, Breaker and Tester of all things UI'
+      />
+      <meta
+        name='twitter:description'
+        content='Alvin Sovereign is a London-Based Web Developer specialising in the front end.'
+      />
+      <meta name='twitter:image' content='/images/open_graph_headshot.jpg' />
     </NextHead>
   );
 };
